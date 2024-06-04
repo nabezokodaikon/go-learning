d24f57fabe1785e2f23e9b4d1c22e32063c02b30
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-06-04 22:11:46 +0900

    Add struct

--- src/main.go
+++ src/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+  "encoding/json"
   // "fmt"
   // "github.com/junegunn/fzf/src/algo"
   // "github.com/junegunn/fzf/src/util"
+  // "strconv"
   "syscall/js"
 )
 
-func add(this js.Value, args []js.Value) interface{} {
-  var arr interface{} = args[0]
-  result := make([]js.Value, 0, 2)
-  for v := range arr.([]js.Value) {
-    result = append(result, (v))
-  } 
-  
-  // b := args[1].Int()
-  return js.ValueOf(result)
+type Animal struct {
+  Name string
+  Hoge int
+}
+
+func find(this js.Value, args []js.Value) interface{} {
+  length := args[1].Int()
+  jsArray := js.Global().Get("Array").New(length)
+  for i := 0; i < length; i++ {
+    v := args[i + 2];
+    str := js.Global().Get("JSON").Call("stringify", v).String()
+    var dst Animal
+    json.Unmarshal([]byte(str), &dst)
+    jsObj := js.Global().Get("Object").New()
+    jsObj.Set("Name", dst.Name)
+    jsObj.Set("Hoge", 777)
+    jsArray.SetIndex(i, jsObj)
+  }
+
+  return jsArray
 }
 
 func main() {
   c := make(chan struct{}, 0)
-  js.Global().Set("add", js.FuncOf(add))
+  js.Global().Set("find", js.FuncOf(find))
   <-c
 
   // input := util.RunesToChars([]rune("github.com/wantedly/wantedly"))
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import "./wasm_exec.js";
-import { Go } from "https://deno.land/x/godeno@v0.7.0/mod.ts";
+//import { Go } from "https://deno.land/x/godeno@v0.7.0/mod.ts";
+
+type Animal = {
+  Name: string;
+  Hoge: number;
+}
 
 const go = new (globalThis as any).Go();
 
@@ -9,6 +14,14 @@ const wasmInstance = await WebAssembly.instantiate(wasmModule, go.importObject);
 
 go.run(wasmInstance);
 
-const result = (globalThis as any).add(["hello", "world"]);
+function receive(array: any) {
+  //console.log("Received array from Go:", array);
+}
+
+//const array = ["aaa", "bbb", "ccc"];
+const array: Array<Animal> = [{name: "aaa", hoge: 1}, {name: "bbb", hoge: 2}]
+const length = array.length;
+const params = [receive, length, ...array]
+const result = (globalThis as any).find(...params);
 console.log(result);
 
