0cf19872d72a3ad4bcbcf96c94b5fc10569d895a
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-06-06 07:27:03 +0900

    fzfパッケージをwasmにすることは不可能

--- src/main.go
+++ src/main.go
@@ -1,63 +1,52 @@
 package main
-
 import (
   "encoding/json"
   "fmt"
   // "golang.org/x/exp/maps"
-  // "github.com/junegunn/fzf/src/algo"
-  // "github.com/junegunn/fzf/src/util"
+  "github.com/junegunn/fzf/src/algo"
+  "github.com/junegunn/fzf/src/util"
   // "strconv"
   "syscall/js"
 )
 
-type Animal struct {
-  Name string
-  Hoge int
-}
-
 type ItemHighlight struct {
-  name string
-  hl_group string
-  col int
-  width int
+  Name string
+  Hl_group string
+  Col int
+  Width int
 }
 
 type ItemStatus struct {
-  size int
-  time int
-}
-
-type Record struct {
-  int
-  string
+  Size int
+  Time int
 }
 
 type DduItem struct {
-  word string
-  display string
+  Word string
+  Display string
   // action ActionData
   // data unknown
-  highlights []ItemHighlight
-  status ItemStatus
-  kind string
-  level int
-  treePath string
-  isExpanded bool 
-  isTree bool
+  Highlights []ItemHighlight
+  Status ItemStatus
+  Kind string
+  Level int
+  TreePath string
+  IsExpanded bool 
+  IsTree bool
 
-  matcherKey string
-  __sourceIndex int
-  __sourceName string
-  __level int
-  __expanded bool
-  __columnTexts Record
-  __groupedPath string
+  MatcherKey string
+  SourceIndex int `json:"__sourceIndex"`
+  SourceName string `json:"__sourceName"`
+  Level_ int `json:"__level"`
+  Expanded bool `json:"__expanded"`
+  ColumnTexts map[int]string `json:"__columnTexts"`
+  GroupedPath string `json:"__groupedPath"`
 }
 
 type FindResult struct {
   Score int
   Positions []int
-  item DduItem
+  Item DduItem
 }
 
 // func structure(this js.Value, args []js.Value) interface{} {
@@ -89,18 +78,43 @@ type FindResult struct {
 //   return jsObj
 // }
 
+func toJson(findResult FindResult) interface{} {
+  return map[string]interface{}{
+    "user": findResult.Item.Word,
+  }
+}
+
 func find(this js.Value, args []js.Value) interface{} {
   input := args[0]
   fmt.Println(input)
   length := args[1].Int()
-  // jsArray := js.Global().Get("Array").New(length)
+  fmt.Println(length)
+  jsArray := js.Global().Get("Array").New(length)
+  fmt.Println(jsArray)
+  var jsonSlice []interface{}
+  fmt.Println(jsonSlice)
   for i := 0; i < length; i++ {
-    v := args[i + 1];
+    v := args[i + 2]
     str := js.Global().Get("JSON").Call("stringify", v).String()
-    var dst DduItem
-    json.Unmarshal([]byte(str), &dst)
-    fmt.Println(dst)
-    // jsObj := js.Global().Get("Object").New()
+    fmt.Println(str)
+    var item DduItem
+    json.Unmarshal([]byte(str), &item)
+    fmt.Println(item)
+
+    word := util.RunesToChars([]rune(item.Word))
+    util.RunesToChars([]rune(item.Word))
+    inputRune := []rune(input)
+    slab := util.MakeSlab(100*1024, 2048)
+    result, positions := algo.FuzzyMatchV2(false, true, true, &word, inputRune, true, slab)
+    findResult := FindResult {
+      Score: result.Score,
+      Positions: *positions,
+      Item: item,
+    }
+    findResultJson := toJson(findResult)
+    jsonSlice = append(jsonSlice, findResultJson) 
+
+
     // jsObj.Set("Name", dst.Name)
     // jsObj.Set("Hoge", 777)
     // jsArray.SetIndex(i, jsObj)
@@ -111,7 +125,6 @@ func find(this js.Value, args []js.Value) interface{} {
 
 func main() {
   c := make(chan struct{}, 0)
-  // js.Global().Set("structure", js.FuncOf(structure))
   js.Global().Set("find", js.FuncOf(find))
   <-c
 
